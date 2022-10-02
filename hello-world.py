d75e7ee65f5d0b4a41639df6015e7f5a9c0d8626
EC2 Default User <ec2-user@ip-172-31-93-95.ec2.internal>
2022-10-02 12:23:30 +0000

    updated hello world

--- hello-world.py
+++ hello-world.py
@@ -1,1 +1,1 @@
-print('Hello World for Jenkins Job tis my first jenkins job 2022 my best project from 2022 clarusways')
+print('Hello World for Jenkins Job tis my first jenkins job 2022 my best project from 2022 clarusways jesus is the only way')
